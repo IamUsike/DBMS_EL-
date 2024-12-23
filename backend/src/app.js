39b9d4d458a30475796acfdb5bd6da0c9664c4fd
musick <sathwikthanmay4@gmail.com>
2024-12-23 17:52:12 +0530

    allowed cross origin requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,11 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+})) 
+
 app.use(express.json({ limit: "16kb" })); //in forms
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //from url
 app.use(express.static("public")); //some assets which can be seen by everyone
